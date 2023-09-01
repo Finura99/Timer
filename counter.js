b1450ae4fff10299c8891b4c0a2681bef7b7a571
Arya <32606430+Finura99@users.noreply.github.com>
2023-09-01 16:27:14 +0100

    Updated Refactor

--- counter.js
+++ counter.js
@@ -1,35 +1,27 @@
 function updateCountdown() {
-  const second = 1000,
-  minute = second * 60,
-  hour = minute * 60,
-  day = hour * 24,
-  month = day * 30;  
-  
+
   const currentDate = new Date();
-  let targetDay = 76,
-      targetMonth = 0;
+  let targetDay = 5,
+      targetMonth = 5;
  //console.log(currentDate)
 
-  const targetDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + targetMonth, targetDay);
+  const targetDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + targetMonth, targetDay),
+        timeLeft = targetDate - currentDate;
   //console.log(targetDate) 
   // parameters inside of date constructor includes the year, month and day which is customisable
-  const timeLeft = targetDate - currentDate;
-  
-  //difference between the current and target
-  
-  
-  
-  const days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  const months = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 30)),
+        days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
   //Math calculations assigned to variables
 
-  document.getElementById('months').textContent = Math.floor(timeLeft / (month)) + ":",
-  document.getElementById('days').textContent = Math.floor((timeLeft % (month)) / (day)) + ":",
-  document.getElementById('hours').textContent = Math.floor((timeLeft % (day)) / (hour)) + ":",
-  document.getElementById('minutes').textContent = Math.floor((timeLeft % (hour)) / (minute)) + ":",
-  document.getElementById('seconds').textContent = Math.floor(timeLeft % (minute) / second);
+  document.getElementById('months').textContent = months,
+  document.getElementById('days').textContent = days,
+  document.getElementById('hours').textContent = hours,
+  document.getElementById('minutes').textContent = minutes,
+  document.getElementById('seconds').textContent = seconds
   //update the html with DOM manipulation 
 
   const eventButton = document.getElementById("button")
@@ -49,60 +41,6 @@ function updateCountdown() {
   }
 }
 
-
-// Call the function initially and update every second
+// Call the function and update every second (1000 milliseconds)
 updateCountdown();
 setInterval(updateCountdown,1000);
-
-
-
-
-
-// //Assiging the variables so that they will replace the string to a singular noun of it.
-
-// function updateCountdown() {
-//   const second = 1000,
-//     minute = second * 60,
-//     hour = minute * 60,
-//     day = hour * 24,
-//     month = 30;
-
-//   let today = new Date(),
-//     dd = String(today.getDate()).padStart(2, "0"), //e.g. 01 for january
-//     mm = String(today.getDate() + 1).padStart(2, "0"), //e.g. 06 for june
-//     yy = today.getFullYear();
-
-//     console.log(today);
-    
-//     today = dd + "/" + mm + "/" + yy; //reassigned the format of date
-//     chooseDate = "15/10/",
-//     targetEvent = chooseDate + yy;
-
-
-//     console.log(today);
-//     console.log(targetEvent);
-  
-//   const countDown = new Date(targetEvent).getTime(),
-//       x = setInterval(function() {    
-//         //console.log(countDown);
-
-//         const now = new Date().getTime()
-//               timeLeft = countDown - now;
-//           document.getElementById("months").innerText = Math.floor(timeLeft / (month)),
-//           document.getElementById("days").innerText = Math.floor(timeLeft / (day)),
-//           document.getElementById("hours").innerText = Math.floor((timeLeft % (day)) / (hour)),
-//           document.getElementById("minutes").innerText = Math.floor((timeLeft % (hour)) / (minute)),
-//           document.getElementById("seconds").innerText = Math.floor((timeLeft % (minute)) / second);
-//         //do something later when date is reached
-//         if (timeLeft < 0) {
-//           document.getElementById("").innerText = "It's my birthday!";
-//           document.getElementById("countdown").style.display = "none";
-//           document.getElementById("content").style.display = "block";
-//           clearInterval(x);
-//         }
-//         //seconds
-//       }, 1000)
-//   };
-
-//   updateCountdown();
-  
