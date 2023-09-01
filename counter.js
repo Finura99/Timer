1d3633129ebe32b95c1e7f86afa3886de26eca8b
Arya <32606430+Finura99@users.noreply.github.com>
2023-09-01 10:16:59 +0100

    Revert "Not official"
    This reverts commit d1c8bfb57544e813e0b1ddb525359859d4939549.
    dddd

--- counter.js
+++ counter.js
@@ -2,9 +2,8 @@
 //Assiging the variables so that they will replace the string to a singular noun of it.
 
 function updateCountdown() {
-    const currentDate = new Date(),
-        targetDate = new Date(currentDate.getFullYear(), 
-                    currentDate.getMonth() + 1, 0); 
+    const currentDate = new Date();
+    const targetDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 10, 1);
 
     // parameters inside of date constructor includes the year, month and day which is customisable
     const timeLeft = targetDate - currentDate;
@@ -25,8 +24,34 @@ function updateCountdown() {
     document.getElementById('seconds').textContent = seconds;
     //update the html with DOM manipulation 
 
-    
+    let singleMonth = "MONTH";
+    let singleDay = "DAY";
+    let singleHour = "HOUR";
+    let singleMinute = "MINUTE";
+    let singleSecond = "SECOND";
     //assigning strings to singular nouns
+
+    if (months <= 1) {
+        document.getElementById("singlemonth").textContent =  singleMonth;
+    }
+
+    if (days <= 1) {
+        document.getElementById("singleday").textContent = singleDay;
+    } else {
+        document.getElementById("singlesecond").textContent = "DAYS";
+    }
+
+    if (hours <= 1) {
+        document.getElementById("singlehour").textContent = singleHour;
+    } else {
+        document.getElementById("singlehour").textContent = "HOURS";
+    } 
+
+     if (seconds <= 1) {
+         document.getElementById("singlesecond").textContent = singleSecond;
+    } else {
+        document.getElementById("singlesecond").textContent = "SECONDS";
+    }
 
     const hideMonth = document.getElementById("hide-month");
     const hideDay = document.getElementById("hide-day");
@@ -45,44 +70,3 @@ function updateCountdown() {
 updateCountdown();
 setInterval(updateCountdown, 1000);
 //used dry principle to cut down 10 lines of code
-
-// function updateCountdown() {
-//     const second = 1000,
-//           minute = second * 60,
-//           hour = minute * 60,
-//           day = hour * 24,
-//           month = day * 30
-//     let today = new Date(),
-//         dd = today.getDate(),
-//         mm = today.getMonth(),
-//         yyyy = today.getFullYear(),
-//         nextYear = yyyy,
-//         dayMonth = "1/29/",
-//         birthday = dayMonth + yyyy;
-    
-//     today = mm + "/" + dd + "/" + yyyy;
-//     if (today > birthday) {
-//       birthday = dayMonth + nextYear;
-//     }
-    
-//     const countDown = new Date(birthday).getTime(),
-//         x = setInterval(function() {    
-  
-//           const now = new Date().getTime(),
-//                 distance = countDown - now;
-  
-//                 document.getElementById("days").innerText = Math.floor(distance / (day)),
-//                 document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-//             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-//             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-//           //do something later when date is reached
-//           if (distance < 0) {
-//             document.getElementById("headline").innerText = "It's my birthday!";
-//             document.getElementById("countdown").style.display = "none";
-//             document.getElementById("content").style.display = "block";
-//             clearInterval(x);
-//           }
-//           //seconds
-//         }, 0)
-//     };
